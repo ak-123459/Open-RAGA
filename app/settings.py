41a4fe04f22d629d86af2821fa2d4b30b1a1d349
Akash Prasad Mishra <139432447+ak-123459@users.noreply.github.com>
2025-06-17 23:43:15 +0530

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -2,9 +2,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MODEL_CONFIG_PATH = BASE_DIR /"config" /  "prod" / "model_config.yaml"
-DOCS_PATH = BASE_DIR / "data" / "preprocessed" 
-EMBD_MODEL_DIR = BASE_DIR / "src" / "embedder" / "model_checkpoints" 
-VECTOR_STORE_PATH = BASE_DIR / "data" / "vector_db" / "knowledge_base" 
-PROMPT_TEMPLATES_PATH = BASE_DIR / "generation" / "prompt_templates.yaml"
+MODEL_CONFIG_PATH = BASE_DIR / "app"  / "config" /  "prod" / "model_config.yaml"
+DOCS_PATH = BASE_DIR /  "app" /"data" / "preprocessed" 
+EMBD_MODEL_DIR = BASE_DIR / "app" / "src" / "embedder" / "model_checkpoints" 
+VECTOR_STORE_PATH = BASE_DIR / "app" / "data" / "vector_db" / "knowledge_base" 
+PROMPT_TEMPLATES_PATH = BASE_DIR / "app" / "generation" / "prompt_templates.yaml"
 
